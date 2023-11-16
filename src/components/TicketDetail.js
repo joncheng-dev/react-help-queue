8323c9e736cef5efdad5a4b7e4cabadda276d9b3
Jonathan Cheng <jcheng461@gmail.com>
2023-11-16 12:04:16 -0800

    add README template for project

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function TicketDetail(props) {
-  const { ticket, onClickingDelete, onClickingEdit } = props;
+  const { ticket, onClickingDelete } = props;
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
@@ -12,7 +12,7 @@ function TicketDetail(props) {
       <p>
         <em>{ticket.issue}</em>
       </p>
-      <button onClick={() => onClickingEdit()}>Edit</button>
+      <button onClick={props.onClickingEdit}>Edit</button>
       <button onClick={() => onClickingDelete(ticket.id)}>Delete Ticket </button>
       <hr />
     </React.Fragment>
