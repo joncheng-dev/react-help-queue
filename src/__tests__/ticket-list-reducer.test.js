19911be5f77b8af1c93548502eea09c152cf2b1e
Jonathan Cheng <jcheng461@gmail.com>
2023-11-26 14:00:33 -0800

    add test2 for ticket-list-reducer; pass. Add test3; good fail

--- src/__tests__/ticket-list-reducer.test.js
+++ src/__tests__/ticket-list-reducer.test.js
@@ -1,7 +1,66 @@
 import ticketListReducer from "../reducers/ticket-list-reducer";
 
 describe("ticketListReducer", () => {
+  let action;
+  const ticketData = {
+    names: "Moses and Ian",
+    location: "cello-1",
+    issue: "Redux action is not working correctly.",
+    id: 1,
+  };
+
+  const currentState = {
+    1: {
+      names: "Drake and Josh",
+      location: "cello-2",
+      issue: "Git history not aligned with GitHub.",
+      id: 1,
+    },
+    2: {
+      names: "Spongebob and Patrick",
+      location: "drum-1",
+      issue: "Reducer has side effects.",
+      id: 2,
+    },
+  };
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
+  });
+
+  test("Should successfully add new ticket data to mainTicketList", () => {
+    const { names, location, issue, id } = ticketData;
+    action = {
+      type: "ADD_TICKET",
+      names: names,
+      location: location,
+      issue: issue,
+      id: id,
+    };
+    expect(ticketListReducer({}, action)).toEqual({
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id,
+      },
+    });
+  });
+
+  test("Should successfully delete a ticket", () => {
+    action = {
+      type: "DELETE_TICKET",
+      id: 1,
+    };
+    expect(
+      ticketListReducer(currentState, action).toEqual({
+        2: {
+          names: "Spongebob and Patrick",
+          location: "drum-1",
+          issue: "Reducer has side effects.",
+          id: 2,
+        },
+      })
+    );
   });
 });
