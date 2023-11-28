b7cc9846d3b04c9b2be3fc6e7d6c41ed61ab92d0
Jonathan Cheng <jcheng461@gmail.com>
2023-11-27 20:56:28 -0800

    refactor formVisibleOnPage state slice to be handled by Redux

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -10,7 +10,6 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false,
       selectedTicket: null,
       editing: false,
     };
@@ -19,12 +18,15 @@ class TicketControl extends React.Component {
   handleClick = () => {
     if (this.state.selectedTicket != null) {
       this.setState({
-        formVisibleOnPage: false,
         selectedTicket: null,
         editing: false,
       });
     } else {
-      this.setState((prevState) => ({ formVisibleOnPage: !prevState.formVisibleOnPage }));
+      const { dispatch } = this.props;
+      const action = {
+        type: "TOGGLE_FORM_VISIBILITY",
+      };
+      dispatch(action);
     }
   };
 
@@ -39,7 +41,10 @@ class TicketControl extends React.Component {
       issue: issue,
     };
     dispatch(action);
-    this.setState({ formVisibleOnPage: false });
+    const action2 = {
+      type: "TOGGLE_FORM_VISIBILITY",
+    };
+    dispatch(action2);
   };
 
   handleChangingSelectedTicket = (id) => {
@@ -92,7 +97,7 @@ class TicketControl extends React.Component {
         <TicketDetail ticket={this.state.selectedTicket} onClickingDelete={this.handleDeletingTicket} onClickingEdit={this.handleEditClick} />
       );
       buttonText = "Return to Ticket List";
-    } else if (this.state.formVisibleOnPage) {
+    } else if (this.props.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />;
       buttonText = "Return to Ticket List";
     } else {
@@ -111,11 +116,13 @@ class TicketControl extends React.Component {
 
 TicketControl.propTypes = {
   mainTicketList: PropTypes.object,
+  formVisibleOnPage: PropTypes.bool,
 };
 
 const mapStateToProps = (state) => {
   return {
-    mainTicketList: state,
+    mainTicketList: state.mainTicketList,
+    formVisibleOnPage: state.formVisibleOnPage,
   };
 };
 
